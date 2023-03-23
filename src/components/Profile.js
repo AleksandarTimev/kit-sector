b71e1b071fdce7913b9acd60221963588cf6514a
AleksandarTimev <alex_0024@abv.bg>
2023-03-23 13:27:25 +0200

    profile page css

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,7 +3,7 @@ import { collection, query, where, getDocs } from "firebase/firestore";
 import { auth, db } from "../firebase.js";
 import { useNavigate } from "react-router-dom";
 import { kitService } from "../services/kitService.js";
-import "../public/css/NotFound.css";
+import "../public/css/Profile.css";
 
 export const Profile = () => {
   const [user, setUser] = useState(null);
@@ -14,7 +14,10 @@ export const Profile = () => {
     const authListener = auth.onAuthStateChanged((user) => {
       if (user) {
         setUser(user);
-        const q = query(collection(db, "shirts"), where("userId", "==", user.uid));
+        const q = query(
+          collection(db, "shirts"),
+          where("userId", "==", user.uid)
+        );
         getDocs(q)
           .then((querySnapshot) => {
             const userKits = [];
@@ -35,42 +38,50 @@ export const Profile = () => {
     };
   }, []);
 
-  useEffect(() => {
-    if (user) {
-    //   console.log("Welcome, " + user.email + "!");
-    }
-  }, [user]);
-  console.log(kits);  // log the entire kits array
-  console.log(kits.map(kit => kit.id));
   return (
-<div className="container">
-  <h2>Profile:</h2>
-  {/* <p>{user.email}</p> */}
-  <h3>My Kits:</h3>
-  {kits.length > 0 ? (
-    <ul className="kits-grid">
-      {kits.map((kit) => (
-        <li className="kit-details li-catalog" key={kit.id}>
-          <div>
-            <h4>{kit.name}</h4>
-            <img src={kit.imageUrl} alt={kit.name} />
-          </div>
-          <div className="kit-buttons">
-            <button
-              type="button"
-              className="btn btn-secondary mx-2"
-              onClick={() => kitService.handleDetailsKit(kit.id, navigate)}
-            >
-              Details
-            </button>
-          </div>
-        </li>
-      ))}
-    </ul>
-  ) : (
-    <p>You have not uploaded any kits yet.</p>
-  )}
-</div>
+    <div className="container">
+      <div className="profile-pic">
+        <h2>Profile</h2>
+        <img
+          src="https://cdn4.iconfinder.com/data/icons/basics-set-2/100/User_Profile-512.png"
+          alt="Avatar"
+          width="75"
+          height="75"
+        />
+            {user && <p>Email: {user.email}</p>}
+      </div>
+      <div className="profile">
+        <div className="avatar"></div>
+        <div className="profile-kits">
+          <h3>Your Kits:</h3>
+          {kits.length > 0 ? (
+            <ul className="kits-grid">
+              {kits.map((kit) => (
+                <li className="kit-details li-catalog" key={kit.id}>
+                  <div>
+                    <h4>{kit.name}</h4>
+                    <img src={kit.imageUrl} alt={kit.name} />
+                  </div>
+                  <div className="kit-buttons">
+                    <button
+                      type="button"
+                      className="btn btn-secondary mx-2"
+                      onClick={() =>
+                        kitService.handleDetailsKit(kit.id, navigate)
+                      }
+                    >
+                      Details
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>You have not uploaded any kits yet.</p>
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
