d2d46226918176ce8770cb3c5ac16b6ddc5a1fa6
Aleksandar Timev <123753252+AleksandarTimev@users.noreply.github.com>
2023-10-02 16:06:47 +0300

    navigation test update

--- cypress/e2e/Navigation/navigation.spec.cy.js
+++ cypress/e2e/Navigation/navigation.spec.cy.js
@@ -1,10 +1,20 @@
 describe("Navigation Page", () => {
-  const navigationLinks = [
+  const navigationLinksOut = [
     "Home",
     "Catalog",
     "Contact Us",
     "Login",
     "Register",
+  ];
+
+  const navigationLinksIn = [
+    "Home",
+    "Catalog",
+    "Contact Us",
+    "Upload",
+    "My Profile",
+    "My Cart",
+    // "Log Out"
   ];
 
   beforeEach(() => {
@@ -17,13 +27,39 @@ describe("Navigation Page", () => {
       .contains("KIT SECTOR");
   });
 
-  it("Should hover over navigation links with a class", () => {
-    navigationLinks.forEach((linkText) => {
+  it("Should hover over navigation links with a class (logged out)", () => {
+    navigationLinksOut.forEach((linkText) => {
       cy.get("a.nav-link")
         .contains(linkText)
         .trigger("mouseover")
         .should("have.css", "color", "rgba(0, 0, 0, 0.5)")
         .click();
     });
+  });
+
+  it("User able to login", () => {
+    cy.loginCommand();
+  });
+
+  it("user should land on home page", () => {
+    cy.homePageLanding();
+  });
+
+  it("Should hover over navigation links with a class (logged in)", () => {
+    cy.wait(1500);
+    navigationLinksIn.forEach((linkText) => {
+      cy.get("a.nav-link")
+        .contains(linkText)
+        .trigger("mouseover")
+        .should("have.css", "color", "rgba(0, 0, 0, 0.5)")
+        .click();
+    });
+  });
+
+  it("User is able to log out", () => {
+    cy.dataCy("cy-logout")
+      .trigger("mouseover")
+      .should("have.css", "color", "rgba(0, 0, 0, 0.5)")
+      .click();
   });
 });
